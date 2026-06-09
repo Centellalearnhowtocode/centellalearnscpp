286733ce59b3490416cd03ca6e1176086dc95630
centellalearnhowtocode <chornay126@gmail.com>
2026-06-09 10:31:17 +0700

    release

---
+++ Classmaterial/Chapter1Lab.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+int main(){
+//Date: 09-June-2024
+//Excercise 1
+string name = "Theara";
+int ID = 12345;
+string major = "IT Engineering";
+
+cout <<"=================================="<< endl;
+cout <<"\tSTUDENT PROFILE" << endl;
+cout <<"=================================="<< endl;
+cout <<"Name :\t " << name << endl;
+cout <<"ID   :\t " << ID << endl;
+cout <<"Major:\t " << major << endl;
+cout <<"==================================\n"<< endl;
+
+//Excercise 2
+string productName = "Book";
+int quantity = 3;
+cout <<"\n\n=================================="<< endl;
+cout <<"\t\tRECEIPT" << endl;
+cout <<"=================================="<< endl;
+cout <<"Product  :\t " << productName << endl;
+cout <<"Quantity :\t " << quantity << endl;
+cout <<"Thank you for shopping!" << endl;
+cout <<"**********************************"<< endl;
+
+//Excercise 3
+string name1 = "Theara"; 
+int age = 21;
+string city = "Phnom Penh";
+
+cout <<"\n\n+------------------------------+"<< endl;
+cout <<"|\tID Card\t\t\t|" << endl;
+cout <<"+------------------------------+"<< endl;
+cout <<"|\tName :\t " << name1 <<"\t\t|" << endl;
+cout <<"|\tAge  :\t " << age <<"\t\t|" << endl;
+cout <<"|\tCity :\t " << city <<"\t|" << endl;
+cout <<"+------------------------------+\n"<< endl;
+
+//Excercise 4
+string studentname ="Theara";
+int studentage = 21;
+int studentID = 12345;
+
+string studentmajor = "IT Engineering";
+string studentuniversity = "RUPP";
+
+cout <<"\n\n============================="<< endl;
+cout <<"\tStudent Information\t\t" << endl;
+cout <<"============================="<< endl;
+cout <<"Peronsal Information" << endl;
+cout <<"Name      :\t " << studentname << endl;
+cout <<"Age       :\t " << studentage << endl;
+cout <<"ID        :\t " << studentID <<"\t" << endl;
+cout <<"Education Information" << endl;
+cout <<"============================="<< endl;
+cout <<"Major     :\t " << studentmajor <<"\t" << endl;
+cout <<"University:\t " << studentuniversity <<"\t" << endl;
+cout <<"============================="<< endl;
+cout <<"End of Report"<< endl;
+cout <<"============================="<< endl;
+
+return 0;
+}
