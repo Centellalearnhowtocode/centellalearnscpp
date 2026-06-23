7816cf044637cde1693ffdfd315debab2cd63552
centellalearnhowtocode <chornay126@gmail.com>
2026-06-23 09:09:03 +0700

    update

---
+++ Youtube Turtorial/typedef.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+typedef string text_t;
+typedef int wholenum; 
+int main (){
+//Today we are going to learn about Typedef
+
+//So what if Tyepdef?? 
+
+//Typedef is used to reserved keyword used to create 
+//an additional name for another datatype. 
+//New identifier for an existing type.
+//Helps with readability and reduces typos.
+// In short it helps you create an alternative name for exising type.
+
+text_t firstname = "Centella";
+text_t firstname2 = "Nobi";
+wholenum age = 19;
+wholenum age2 = 21;
+cout << "Princess\t: " << firstname << '\n';
+cout << "Level\t\t: "<< age << '\n';
+cout << "Princess\t: " << firstname2 << '\n';
+cout << "Level\t\t: " << age2 << '\n';
+
+
+//I spent about 20 mns on reviewing and learning new lesson. 
+//This is my way to fight my lazy ass. 
+//Thank you for watching. See you tomorrow.
+  return 0;
+}
