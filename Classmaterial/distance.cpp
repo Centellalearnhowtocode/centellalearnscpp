3d7cc36acc72826deaa89fc27c192fac46031fbe
chornay126@gmail.com <chornay126@gmail.com>
2026-07-15 14:01:43 +0700

    push

--- Classmaterial/distance.cpp
+++ Classmaterial/distance.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+
 int main(){
 
-  double x, y;
-  std::cout << "Enter the coordinates (x and y): "; //  Adds a helpful prompt
-  std::cin >> x >> y;
+  //Exercise 1
+    double x1, x2, y1, y2;
 
-  return 0;
+    cout << "Enter the coordinates (x1 and y1): " << endl;
+    cin >> x1 >> y1;
+    cout << "Enter the coordinates (x2 and y2): " << endl;
+    cin >> x2 >> y2;
+
+    double result = pow(pow(x2 - x1, 2) + pow(y2 - y1, 2), 0.5);
+
+    cout << "The distance between the two points is " << result << endl;
+
+
+
+    //Excercise 2
+    double side;
+    double area;
+    cout << "Enter the side" << endl;
+    cin >> side;
+    area = (3*pow(3,0.5)/2)*pow(side,2);
+    cout << "The area of the hexagon is" << area << endl;
+    
+    return 0;
+
 }
+
+
