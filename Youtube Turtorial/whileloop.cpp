6c0f08582afe81c4e3d4471a81fa423f1427d9fa
chornay126@gmail.com <chornay126@gmail.com>
2026-08-09 00:36:06 +0700

    Centella

---
+++ Youtube Turtorial/whileloop.cpp
@@ -0,0 +1,47 @@
+/*
+We are making console calculator today.
+*/
+
+#include <iostream>
+using namespace std;
+int main (){
+    char op;
+    double num1;
+    double num2;
+    double result;
+
+    cout << "**************Calculator******************\n";
+    cout << "Enter either (+ - * /)";
+    cin >>op;
+
+    cout <<"Enter #1 :";
+    cin >> num1;
+    
+    cout <<"Enter #2 :";
+    cin >> num2;
+
+    switch (op)
+    {
+    case '+':
+        result = num1 + num2;
+        cout << "Result is " << result << '\n';
+        break;
+    case '-':
+        result = num1 - num2;
+        cout << "Result is " << result << '\n';
+        break;
+    case '*':
+        result = num1 * num2;
+        cout << "Result is " << result << '\n';
+        break;
+    case '/':
+        result = num1 / num2;
+        cout << "Result is " << result << '\n';
+        break;
+    
+    default:
+        cout <<"Please enter either (+ - * /) only";
+        break;
+    }
+    return 0;
+}
